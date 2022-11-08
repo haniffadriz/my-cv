b8f0f61dc06af40e980999beb30609f6805d2adc
haniffadriz <haniffdrz@gmail.com>
2022-11-08 17:18:20 +0700

    thrid commit

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 const tabcontents = document.getElementsByClassName('tab-contents')
-
+const tablinks = document.getElementsByClassName('tab-links')
 const opentab = (tabname) => {
     for(let tabcontent of tabcontents){
         tabcontent.classList.remove('active')
     }
+
+    for(let tablink of tablinks){
+        tablink.classList.remove('active')
+        tablink.addEventListener('click', () => {
+            tablink.classList.add('active')
+        })
+    }
+
+    
 
     document.getElementById(tabname).classList.add('active')
 }
@@ -13,5 +22,13 @@ const menu = document.querySelector('nav ul')
 
 btnMenu.addEventListener('click', () => {
     menu.classList.toggle('active')
+    btnMenu.classList.toggle('active')
 })
+
+const closetab = () => {
+    btnMenu.classList.remove('active')
+    menu.classList.remove('active')
+}
+
+
 
